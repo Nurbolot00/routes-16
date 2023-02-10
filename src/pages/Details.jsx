497f8d0f30b3891368b818ca86634b157e9474f2
Nurbolot <nurbolotu9@gmail.com>
2023-02-10 21:58:16 +0600

    error was completed

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,48 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import Button from '../components/UI/Button';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../components/UI/Button";
 
-const Details = ({products}) => {
-    const {id} = useParams()
-    console.log(id)
-    const navigate = useNavigate()
+const Details = ({ products }) => {
+  const { id } = useParams();
+  console.log(typeof +id);
+  const navigate = useNavigate();
 
-    const [item,setItem] = useState({})
-    console.log(item)
+  const [item, setItem] = useState({});
+  console.log(item);
+  console.log(products);
 
-    const goBackToProductsHandler = () =>{
-        navigate(-1)
-    }
+  const goBackToProductsHandler = () => {
+    navigate(-1);
+  };
 
-    useEffect(() => {
-        const chosenItem = products.find((item) => {
-            console.log(item)
-             return item.id === id
-        })
-        console.log(chosenItem)
-        return setItem(chosenItem)
-    },[products,id])
-    return (
-        <Container>
-            <div>
-                <img src={item} alt={item} />
-            </div>
+  useEffect(() => {
+    const chosenItem = products.find((item) => item.id === Number(id));
+    return setItem(chosenItem);
+  }, [id, products]);
 
-            <div>
-                <h2>{item}</h2>
-                <p><b>Price:</b>${item}</p>
-                <p>{item}</p>
-            </div>
-            <Button onClick={goBackToProductsHandler}>Go Back</Button>
-        </Container>
-    );
+  return (
+    <Container>
+      <div><img src={item.url} alt={item.title} /></div>
+
+      <div>
+        <h2>{item.title}</h2>
+        <p>
+          <b>Price:</b>${item.price}
+        </p>
+        {/* <p>{item}</p> */}
+      </div>
+      <Button onClick={goBackToProductsHandler}>Go Back</Button>
+    </Container>
+  );
 };
 
 export default Details;
 
 const Container = styled.div`
-     width: 100%;
-    padding: 20px 40px;
-    background-color: #c3cfd9;
-`
+  width: 100%;
+  padding: 20px 40px;
+  background-color: #c3cfd9;
+`;
